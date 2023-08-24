d78a4a86d7a574c6617a32b64f13c7213179b686
Ihsan <127175839+ihsanakm@users.noreply.github.com>
2023-08-25 01:13:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+let randomNumber1 = Math.floor(Math.random()*6)+1;
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+
+
+switch(randomNumber1){
+    case 1:
+        document.querySelector(".img1").setAttribute("src","./images/dice1.png");
+        break
+
+    case 2:
+        document.querySelector(".img1").setAttribute("src","./images/dice2.png")
+        break;
+    
+    case 3:
+        document.querySelector(".img1").setAttribute("src","./images/dice3.png")
+        break
+
+    case 4:
+        document.querySelector(".img1").setAttribute("src","./images/dice4.png")
+        break
+
+    case 5:
+        document.querySelector(".img1").setAttribute("src","./images/dice5.png")
+        break
+
+    case 6:
+        document.querySelector(".img1").setAttribute("src","./images/dice6.png")
+        break
+    }
+
+switch(randomNumber2){
+    case 1:
+        {
+        document.querySelector(".img2").setAttribute("src","images/dice1.png")
+        break;
+    }
+    case 2:
+        {
+            document.querySelector(".img2").setAttribute("src","images/dice2.png")
+            break
+        }
+    case 3:
+    {
+        document.querySelector(".img2").setAttribute("src","images/dice3.png")
+        break
+    }
+    case 4:
+    {
+        document.querySelector(".img2").setAttribute("src","images/dice4.png")
+        break
+    }
+    case 5:
+    {
+        document.querySelector(".img2").setAttribute("src","images/dice5.png")
+        break
+    }
+    case 6:
+    {
+        document.querySelector(".img2").setAttribute("src","images/dice6.png")
+        break
+    }
+
+    
+}
+
+if(randomNumber1==randomNumber2){
+    document.querySelector("h1").innerHTML="Match Draw";
+}
+else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 01 is the WINNNER!!"
+}else{
+    document.querySelector("h1").innerHTML="Player 02 is the WINNNER!!"
+}
